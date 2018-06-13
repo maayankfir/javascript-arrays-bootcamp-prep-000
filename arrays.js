647ed4fe7c710109915eec97cb92717f61347fb7
maayankfir <mayankfir@gmail.com>
2018-06-13 14:26:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,3 +5,22 @@ var chocolateBars = [
   'kitkat',
   'skittles',
 ]
+function addElementToBeginningOfArray (array, element) {
+  var newArray = [element, ...array]
+  return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  array.unshift (element)
+  return array
+}
+
+function addElementToEndOfArray (array, element) {
+  var newArray = [...array, element]
+  return newArray
+}
+
+function destructivelyAddElementToEndOfArray (array, element) {
+  array.push (element)
+  return array
+}
