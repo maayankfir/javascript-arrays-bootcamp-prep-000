3c966e8fd9671edb89c4dbf9a253a9b4d349241b
maayankfir <mayankfir@gmail.com>
2018-06-13 15:33:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,6 +35,16 @@ function destructivelyRemoveElementFromBeginningOfArray (array) {
 }
 
 function removeElementFromBeginningOfArray (array) {
-  array = array.slice (1) 
-  return array 
+  array = array.slice (1)
+  return array
+}
+
+function destructivelyRemoveElementFromEndOfArray (array) {
+  array.pop ()
+  return array
+}
+
+function removeElementFromEndOfArray (array){
+  newArray = array.slice (0, array.length - 1)
+  return newArray
 }
