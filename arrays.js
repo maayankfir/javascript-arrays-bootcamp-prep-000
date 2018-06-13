07e44c2fbfa3ac00689f54b645dfdd4a0cef4ba1
maayankfir <mayankfir@gmail.com>
2018-06-13 14:55:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,3 +24,17 @@ function destructivelyAddElementToEndOfArray (array, element) {
   array.push (element)
   return array
 }
+
+function accessElementInArray (array, index) {
+  return array [index] ;
+}
+
+function destructivelyRemoveElementFromBeginningOfArray (array) {
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray (array) {
+  array = array.slice (1) 
+  return array 
+}
